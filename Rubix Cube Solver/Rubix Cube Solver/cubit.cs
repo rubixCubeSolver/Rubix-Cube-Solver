70cdd35f842cf979fb5ff785da1e1201d2b6772f
RubixCubeSolver <cohenron88@gmail.com>
2016-11-08 22:32:25 +0200

    add new files

---
+++ Rubix Cube Solver/Rubix Cube Solver/cubit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rubix_Cube_Solver
+{
+    class cubit
+    {
+        private int _color1;
+        private int _color2;
+        private int _color3;
+
+        public cubit(int color1, int color2)
+        {
+            _color1 = color1;
+            _color2 = color2;
+        }
+
+        public cubit(int color1, int color2, int color3)
+        {
+            _color1 = color1;
+            _color2 = color2;
+            _color3 = color3;
+        }
+    }
+}
